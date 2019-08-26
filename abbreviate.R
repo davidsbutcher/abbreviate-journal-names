1702d646b9d7e3f1a2f64e2cc20f36c9d83c3cef
drdavidbutcher <52331079+drdavidbutcher@users.noreply.github.com>
2019-08-26 15:40:02 -0400

    Initial commit

---
+++ abbreviate.R
@@ -0,0 +1,74 @@
+
+# Packages ------------------------------------------------------------------------------------
+
+library(magrittr)
+library(tools)
+library(glue)
+library(mgsub)
+library(tidyverse)
+
+# Functions -----------------------------------------------------------------------------------
+
+kickout <- function(list) {
+  
+  # This function removes any element from the list of input files
+  # (from root/input) which does not have one of the allowed
+  # extensions or which has "deprecated" in its name
+  
+  allowed_ext <- c("bib", "bibtex")
+  
+  for (i in rev(seq_along(list))) {
+    
+    if (!(tolower(tools::file_ext(list[[i]])) %in% allowed_ext)) {
+      
+      list[[i]] <- NULL 
+      
+    } else if (str_detect(tolower(list[[i]]), fixed("deprecated", TRUE)) == TRUE) {
+      
+      list[[i]] <- NULL 
+      
+    }
+  }
+  
+  return(list)
+}
+
+# Load Files ----------------------------------------------------------------------------------
+
+filelist <- 
+  list.files("input/", recursive = F, include.dirs = F, full.names = T) %>%
+  as.list %>% kickout
+
+bibliolist <- filelist %>% map(read_file)
+
+names(bibliolist) <- filelist %>% as_vector %>% basename
+
+full_names <-
+  read_csv("journal_abbrevs.csv",
+           locale = readr::locale(encoding = "latin1")) %>%
+  drop_na() %>% .$full_name
+
+abbrev_names <-
+  read_csv("journal_abbrevs.csv", 
+           locale = readr::locale(encoding = "latin1")) %>%
+  drop_na() %>% .$abbrev_name
+
+# Replace Full Names --------------------------------------------------------------------------
+
+biblio_output <-
+  bibliolist %>% map(mgsub, 
+                     pattern = full_names,
+                     replacement = abbrev_names,
+                     fixed = TRUE)
+
+
+# Output --------------------------------------------------------------------------------------
+
+biblio_output %>% 
+  map2(filelist, function(bib, file_name) {
+    
+    bib %>% 
+      write_file(glue("output/{file_path_sans_ext(basename(file_name))}_abbreviated.bib"))
+    
+  }
+  )
